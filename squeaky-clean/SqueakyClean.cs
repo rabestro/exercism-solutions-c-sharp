c34f65c60211fd4b6b2c26707ded3e01b4144d0b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-04-05 12:31:25 +0300

    Refactor identifier cleaning function in SqueakyClean project
    The identifier cleaning logic in SqueakyClean.cs is refactored for improved readability and efficiency. The new logic utilizes a switch expression inside a single loop instead of conditional if-else statements. The project also includes task-based tests for the cleaning functions to validate correct behavior.

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -5,21 +5,20 @@ public static class Identifier
     public static string Clean(string identifier)
     {
         var result = new StringBuilder();
+        var isAfterDash = false;
 
-        for (var i = 0; i < identifier.Length; ++i)
-        {
-            var symbol = identifier[i];
-
-            if (symbol is >= 'α' and <= 'ω')
-                continue;
-            if (symbol is '-')
-                symbol = char.ToUpper(identifier[++i]);
-            if (symbol is '\0')
-                result.Append("CTRL");
-            else if (char.IsWhiteSpace(symbol))
-                result.Append('_');
-            else if (char.IsLetter(symbol))
-                result.Append(symbol);
+        foreach (var symbol in identifier)
+        {   
+            result.Append(symbol switch
+            {
+                >= 'α' and <= 'ω' => default,
+                _ when isAfterDash => char.ToUpperInvariant(symbol),
+                _ when char.IsWhiteSpace(symbol) => '_',
+                _ when char.IsControl(symbol) => "CTRL",
+                _ when char.IsLetter(symbol) => symbol,
+                _ => default
+            });
+            isAfterDash = symbol.Equals('-');
         }
 
         return result.ToString();
