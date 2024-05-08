65d03d4d157ba9c0233ffbe1e80d09f4e6e4dd5a
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-05-08 20:53:28 +0300

    Update RobotName to ensure thread safety
    The RobotName method was updated to ensure the uniqueness of the Robot names in a multithreaded environment. The "_currentId" increment operation was moved to an Interlocked.Increment method inside the RandomName method, which guarantees execution to be an atomic (i.e., uninterruptible) operation, ensuring that no two threads can get the same ID.

--- robot-name/RobotName.cs
+++ robot-name/RobotName.cs
@@ -1,18 +1,19 @@
 using System;
+using System.Threading;
 
 public class Robot
 {
     private static int _currentId;
 
-    public Robot() => RandomName(++_currentId);
+    public Robot() => RandomName();
 
     public string Name { get; private set; }
 
-    public void Reset() => RandomName(++_currentId);
+    public void Reset() => RandomName();
 
-    private void RandomName(int seed)
+    private void RandomName()
     {
-        var random = new Random(seed);
+        var random = new Random(Interlocked.Increment(ref _currentId));
         var firstLetter = (char)random.Next('A', 'Z' + 1);
         var secondLetter = (char)random.Next('A', 'Z' + 1);
         var number = random.Next(1000).ToString("D3");
